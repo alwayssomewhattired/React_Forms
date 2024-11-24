83ed83aa95741550ab078139d332cf639a068ca9
Zachary Malinka <zacharymalinka@gmail.com>
2024-11-23 19:51:41 -0500

    everything should work

--- React_Forms/src/components/SignUpForm.jsx
+++ React_Forms/src/components/SignUpForm.jsx
@@ -1,26 +1,55 @@
 import { useState } from "react";
 
-function SignUpForm() {
+function SignUpForm({ setToken }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
   async function handleSubmit(event) {
     event.preventDefault();
-    console.log("hello");
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            username,
+            password,
+          }),
+        }
+      );
+      const result = await response.json();
+      setToken(result.token);
+    } catch (error) {
+      setError(error.message);
+    }
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input value={username} onChange={(e) => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input value={password} onChange={(e) => setPassword(e.target.value)} />
-      </label>
-      <button>Submit</button>
-    </form>
+    <>
+      <h2>Sign Up</h2>
+
+      {error && <p>{error}</p>}
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            minLength={8}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            minLength={8}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </>
   );
 }
 
